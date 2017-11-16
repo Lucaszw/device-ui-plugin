3395c23ebd3970d2a053574ad00f201b7cdc4b92
lucaszw <lucas.zeer@gmail.com>
2017-11-16 17:51:33 -0500

    move ui controlls out of device-controller and into device-ui-plugin

--- src/device-ui-plugin.js
+++ src/device-ui-plugin.js
@@ -1,13 +1,17 @@
 require('style-loader!css-loader!jquery-contextmenu/dist/jquery.contextMenu.css');
 const $ = require('jquery'); require('jquery-contextmenu');
+const Key = require('keyboard-shortcut');
 const Dat = require('dat.gui/build/dat.gui');
 const uuid = require('uuid/v4');
+const yo = require('yo-yo');
 
 const SVGRenderer = require('@microdrop/device-controller/src/svg-renderer');
 
 const DeviceController = require('@microdrop/device-controller/src/device-controller');
 const MicrodropAsync = require('@microdrop/async');
 const UIPlugin = require('@microdrop/ui-plugin');
+
+const DIRECTIONS = {LEFT: "left", UP: "up", DOWN: "down", RIGHT: "right"};
 
 class DeviceUIPlugin extends UIPlugin {
   constructor(elem, focusTracker) {
@@ -20,9 +24,19 @@ class DeviceUIPlugin extends UIPlugin {
   listen() {
     this.on("updateRequest", this.onUpdateRequest.bind(this));
 
-    // XXX: Sometimes updateRequest doesn't fire on page reload
+    // XXX: Sometimes updateRequest doesn't fire on page reload (thus force it with timeout)
     setTimeout(()=>this.trigger("updateRequest"), 1000);
-    // await this.render();
+
+    Key("left", this.move.bind(this, DIRECTIONS.LEFT));
+    Key("right", this.move.bind(this, DIRECTIONS.RIGHT));
+    Key("up", this.move.bind(this, DIRECTIONS.UP));
+    Key("down", this.move.bind(this, DIRECTIONS.DOWN));
+  }
+
+  move(...args) {
+    if (!this.controls) return;
+    if (document.activeElement != this.element) return;
+    this.controls.electrodeControls.move(...args);
   }
 
   onUpdateRequest(msg) {
@@ -33,7 +47,7 @@ class DeviceUIPlugin extends UIPlugin {
   }
 
   contextMenuClicked(key, options) {
-    if (!this.controls) return;
+    if (!this.controls) return true;
     const microdrop = new MicrodropAsync();
     switch (key) {
       case "clearElectrodes":
@@ -49,9 +63,14 @@ class DeviceUIPlugin extends UIPlugin {
         this.controls.routeControls.trigger("execute-route", {key, options});
         break;
       case "executeRoutes":
-        this.controls.routeControls.trigger("execute-route", {key, options});
+        // XXX: async methods don't seem to work with jquery context menu
+        // (using promises instead)
+        microdrop.routes.routes().then((routes) => {
+          microdrop.routes.execute(routes);
+        });
         break;
     }
+    return true;
   }
 
   async render() {
@@ -73,13 +92,15 @@ class DeviceUIPlugin extends UIPlugin {
       this.controls = controls;
       this.gui = gui;
       this.contextMenu = contextMenu;
+
+      this.element.onclick = () => this.element.focus();
+
     } catch (e) {
       console.error(LABEL, e.toString());
     }
   }
 
   static CreateContextMenu(element, callback) {
-    const microdrop = new MicrodropAsync();
     const id = uuid();
     element.setAttribute("id", id);
     return $.contextMenu({
@@ -92,7 +113,10 @@ class DeviceUIPlugin extends UIPlugin {
             executeRoute: {name: "Execute Route"},
             "sep2": "---------",
             clearRoutes: {name: "Clear Routes"},
-            executeRoutes: {name: "Execute Routes"}
+            executeRoutes: {name: "Execute Routes"},
+            "sep3": "---------",
+            "select1": {name: "Select Electrode: Shift-Click", disabled: true},
+            "select2": {name: "Select Route: Alt-Click", disabled: true}
         }
     });
   }
@@ -100,8 +124,8 @@ class DeviceUIPlugin extends UIPlugin {
   static CreateDatGUI(container=null, menu={}) {
     if (!container) container = document.body;
     const gui = new Dat.GUI({autoPlace: false});
-    gui.add(menu.cameraControls || cameraControls, 'enableRotate');
-    gui.add(menu.videoControls || videoControls, "display_anchors");
+    gui.add(menu.cameraControls, 'enableRotate');
+    gui.add(menu.videoControls, "display_anchors");
     gui.domElement.style.position = "absolute";
     gui.domElement.style.top = "0px";
     gui.domElement.style.right = "0px";
